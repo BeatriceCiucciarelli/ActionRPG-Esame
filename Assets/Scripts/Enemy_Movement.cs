e742b0712aab4f366861cad9703ba1fb72601569
BeatriceCiucciarelli <bea.ciucciarelli@stud.uniroma3.it>
2026-08-24 18:42:39 +0200

    Enemy Update: add Idle/Walk animations
    Added enemy Idle and Walk animations and implemented logic to switch states based on movement.

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -1,45 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_Movement : MonoBehaviour
 {
     public float speed;
     private bool isChasing;
+    private int facingDirection = -1;
 
     private Rigidbody2D rb;
     private Transform player;
+    private Animator anim;   // <-- aggiunto
 
-    // Start is called before the first frame update
     void Start()
     {
-        rb= GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();   // <-- aggiunto
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(isChasing==true) {
-            Vector2 direction = (player.position - transform.position).normalized;
-            rb.velocity=direction*speed;
-        }
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.tag=="Player") {
-            if(player==null)
+        if (isChasing && player != null)
+        {
+            if (player.position.x > transform.position.x && facingDirection == -1 ||
+                player.position.x < transform.position.x && facingDirection == 1)
             {
-                player=collision.transform; 
+                Flip();
             }
-            isChasing=true;
-        } 
+
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.velocity = direction * speed;
+
+            // stessa logica del player: passo i valori assoluti all'Animator
+            anim.SetFloat("horizontal", Mathf.Abs(direction.x));
+            anim.SetFloat("vertical", Mathf.Abs(direction.y));
+        }
+        else
+        {
+            // fermo: azzero velocità e animazione (torna in idle)
+            rb.velocity = Vector2.zero;
+            anim.SetFloat("horizontal", 0);
+            anim.SetFloat("vertical", 0);
+        }
     }
 
-    private void OnTriggerExit2D(Collider2D collision) {
-        if(collision.gameObject.tag=="Player") {
-            rb.velocity=Vector2.zero;
-            isChasing=false;
-        } 
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (player == null)
+            {
+                player = collision.transform;
+            }
+            isChasing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isChasing = false;
+        }
     }
 }
