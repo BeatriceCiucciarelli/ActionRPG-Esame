27d482940d786843bd71cbeee0db716b177b85cc
BeatriceGhigi <beaghigi44@gmail.com>
2026-08-26 20:29:02 +0200

    skill tree.2

---
+++ Assets/Scripts/SkillTree/SkillSlot.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SkillSlot : MonoBehaviour
+{
+    public SkillSO skillSO;
+
+    public int currentLevel;
+    public bool isUnlocked;
+
+    public TMP_Text skillLevelText;
+    public Image skillIcon;
+
+    private void OnValidate(){
+        if(skillSO!=null && skillLevelText != null){
+            UpdateUI();
+        }
+    }
+
+    private void UpdateUI(){
+    
+    skillIcon.sprite=skillSO.skillIcon;
+   
+   if(isUnlocked){
+    
+    skillLevelText.text = currentLevel.ToString() + "/" + skillSO.maxLevel.ToString();
+    skillIcon.color = Color.white;
+
+   } else {
+
+    skillLevelText.text = "Locked";
+    skillIcon.color = Color.grey;
+   }
+
+    }
+}
