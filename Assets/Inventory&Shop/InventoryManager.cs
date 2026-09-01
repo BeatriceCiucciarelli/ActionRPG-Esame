d84a39a8547b7ad87eb0507b8f4bb1fa4346db84
BeatriceGhigi <beaghigi44@gmail.com>
2026-09-01 22:53:12 +0200

    inventory.2

---
+++ Assets/Inventory&Shop/InventoryManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using Unity.VisualScripting;
+
+public class InventoryManager : MonoBehaviour
+{
+    public int gold;
+    public TMP_Text goldText;
+    public InventorySlot[] itemSlots;
+
+   private void OnEnable()
+    {
+        Loot.OnItemLooted += AddItem;
+    
+    } 
+
+    private void OnDisable()
+    {
+        Loot.OnItemLooted -= AddItem;
+    }
+
+
+    public void AddItem(ItemSO itemSO, int quantity)
+    {
+        if (itemSO.isGold)
+        {
+            gold += quantity;
+            goldText.text = gold.ToString();
+        }
+        else
+        {
+            foreach(var slot in itemSlots)
+            {
+                if( slot.itemSO == null)
+                {
+                    slot.itemSO = itemSO;
+                    slot.quantity = quantity;
+                     slot.UpdateUI();
+                     return;
+                }
+                
+            }
+           
+        }
+    }
+
+   
+}
--- Assets/Inventory&Shop/Loot.cs
+++ Assets/Inventory&Shop/Loot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,7 @@ public class Loot : MonoBehaviour
     public Animator anim;
 
     public int quantity;
+    public static event Action<ItemSO, int> OnItemLooted;
 
     private void OnValidate() {
         if(itemSO==null) 
@@ -22,6 +24,7 @@ public class Loot : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.CompareTag("Player")) {
             anim.Play("LootPickup");
+            OnItemLooted?.Invoke(itemSO, quantity);
             Destroy(gameObject, .5f);
         }
     }
